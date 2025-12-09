11d09cd8561a3e4b25d66c63449bd6739dc2aefb
Kelsey Dabrowski <kdabrowski103@gmail.com>
2025-12-08 19:45:51 -0500

    Define other constants and time_horizon() function, add README

--- savings.py
+++ savings.py
@@ -4,7 +4,14 @@ import sys
 # Average retirement age in the US
 RETIREMENT_AGE = 65
 
-# Initialize smaller functions 
+# Average rate of return (100 year average)
+OPTIMIST_ROR = 0.10
+
+# More conservative rate of return (industry suggestion)
+PESSIMIST_ROR = 0.60
+
+
+# Initialize smaller functions
 def get_age():
     age = int(input("How old are you? "))
 
@@ -12,22 +19,33 @@ def get_age():
     if age >= RETIREMENT_AGE:
         print("A little late to be thinking about retirement, isn't it?")
         sys.exit()
-    else: 
+    else:
         return age
 
+
+# Time horizon refers to length of time between now and typical retirement
 def time_horizon(age):
-    ...
-    
-def invested(age, amount):
+    return RETIREMENT_AGE - age
+
+
+def invested(thz, spent):
     ...
 
 
-# Initialize main function 
+# Initialize main function
 def main():
-    # Ask user for age 
+    # Ask user for age, calculate time horizon
     age = get_age()
 
-    # Ask user for how much they spent 
-    # Produce estimated investment profit 
+    thz = time_horizon(age)
+
+    print(age, thz)
+
+    # Ask user for how much they spent
+    # Using float since it's a monetary value
+    spent = float(input("How much money did you just spend? $"))
+
+    # Produce estimated investment profit
+
 
 main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ functions
+def get_age():
